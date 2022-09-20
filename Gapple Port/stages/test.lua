91fd19917693ee5bb7343854c049aa840e0453ef
RubenDatOneBoi <rubedude3@gmail.com>
2022-09-20 21:47:42 +0000

    Update 2 files

--- Gapple Port/stages/test.lua
+++ Gapple Port/stages/test.lua
@@ -1,7 +1,6 @@
 function onCreate()
 	makeLuaSprite('trist', 'trist', -1475, -775);
 	scaleObject('trist', 3, 3);
-    addGlitchEffect('trist', 2,2)
 	addLuaSprite('trist', false)
 	
 	makeLuaSprite('tristan_gang', 'tristan_gang', 100, 130);
@@ -9,4 +8,8 @@ function onCreate()
 	
 	makeLuaSprite('daverson', 'daverson', -375, 100);
 	addLuaSprite('daverson', false)
-    end
+	end
+
+function onStartCountdown()
+	doTweenAngle('anglelele', 'trist', 14040, 205, 'linear');
+end
---
+++ hideTimebar.lua
@@ -0,0 +1,5 @@
+function onCreate()
+    setProperty('timeBarBG.visible', false);
+    setProperty('timeBar.visible', false);
+    setProperty('timeTxt.visible', true);
+end
